653a92350db2ed4bb5281e5e4ab276a207c14f21
lvshun <1294432739@qq.com>
2024-12-24 16:15:42 +0800

    feat: openai配置

--- openai.js
+++ openai.js
@@ -1,6 +1,6 @@
 
 
-import OpenAI from "openai";
+const OpenAI = require("openai") 
 
 const apiKey =
   "sk-zLofzr5n7ae4U5C1Vh2em9Cz4VEVLo2HlHwbRl3Tnpsq87BW";
@@ -9,7 +9,6 @@ const client = new OpenAI({
   apiKey: apiKey, // 混元 APIKey
   baseURL: "https://api.hunyuan.cloud.tencent.com/v1", // 混元 endpoint
 });
-
-export default {
+module.exports = {
   client,
 };
